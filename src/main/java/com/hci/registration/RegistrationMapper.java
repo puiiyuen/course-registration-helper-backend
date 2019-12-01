b82399f02ec6242b333724f152fb02da91f9587b
puiiyuen <peiyuan.chen@smu.ca>
2019-12-01 19:04:28 -0400

    Registration - Add courses - bug - fix

--- src/main/java/com/hci/registration/RegistrationMapper.java
+++ src/main/java/com/hci/registration/RegistrationMapper.java
@@ -6,9 +6,10 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface RegistrationMapper {
 
-    @Insert("INSERT INTO registration (user_id,course_id) SELECT #{userId},#{courseId} " +
+    @Insert("INSERT INTO registration (user_id,course_id) " +
+            "SELECT #{userId},#{courseId} " +
             "FROM dual WHERE NOT EXISTS (SELECT user_id FROM registration " +
-            "WHERE user_id=#{user_id} AND course_id=3{courseId})")
+            "WHERE user_id=#{userId} AND course_id=#{courseId})")
     int addCourse(String userId,String courseId);
 
 }
